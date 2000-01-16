3d773c59e479921ea9acc1272798dec5a49e9de8
reinelt <reinelt@3ae390bd-cb1e-0410-b409-cd5a39f66f1f>
2000-01-16 16:58:50 +0000

    [lcd4linux @ 2000-01-16 16:58:50 by reinelt]
    git-svn-id: https://ssl.bulix.org/svn/lcd4linux/trunk@3 3ae390bd-cb1e-0410-b409-cd5a39f66f1f

--- display.h
+++ display.h
@@ -1,3 +1,34 @@
+/* $Id: display.h,v 1.2 2000/01/16 16:58:50 reinelt Exp $
+ *
+ * framework for device drivers
+ *
+ * Copyright 1999, 2000 by Michael Reinelt (reinelt@eunet.at)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *
+ * $Log: display.h,v $
+ * Revision 1.2  2000/01/16 16:58:50  reinelt
+ * *** empty log message ***
+ *
+ *
+ */
+
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
+
 #define BAR_L 1
 #define BAR_R 2
 #define BAR_U 4
@@ -28,3 +59,5 @@ int lcd_clear (void);
 int lcd_put (int x, int y, char *text);
 int lcd_bar (int type, int x, int y, int max, int len1, int len2);
 int lcd_flush (void);
+
+#endif
