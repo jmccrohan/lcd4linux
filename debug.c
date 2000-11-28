807023c0f882d0ab12e272d84828981548bd5c4e
reinelt <reinelt@3ae390bd-cb1e-0410-b409-cd5a39f66f1f>
2000-11-28 20:20:38 +0000

    [lcd4linux @ 2000-11-28 20:20:38 by reinelt] added debug.c things like that should not hapen. debug.c exists for a few months now, but was never added to CVS. Shit happens....
    git-svn-id: https://ssl.bulix.org/svn/lcd4linux/trunk@72 3ae390bd-cb1e-0410-b409-cd5a39f66f1f

---
+++ debug.c
@@ -0,0 +1,84 @@
+/* $Id: debug.c,v 1.1 2000/11/28 20:20:38 reinelt Exp $
+ *
+ * debug() and error() functions
+ *
+ * Copyright 1999, 2000 by Michael Reinelt (reinelt@eunet.at)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *
+ * $Log: debug.c,v $
+ * Revision 1.1  2000/11/28 20:20:38  reinelt
+ *
+ * added debug.c
+ * things like that should not hapen. debug.c exists for a few months now, but was never added to CVS. Shit happens....
+ *
+ */
+
+/* 
+ * exported functions:
+ *
+ * message (level, format, ...)
+ *   passes the arguments to vsprintf() and
+ *   writes the resulting string either to stdout
+ *   or syslog.
+ *   this function should not be called directly,
+ *   but the macros info(), debug() and error()
+ *
+ */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <syslog.h>
+
+#include "debug.h"
+
+void message (int level, const char *format, ...)
+{
+  va_list ap;
+  char buffer[256];
+  static int log_open=0;
+  extern int debugging;
+  extern int foreground;
+
+  if (level>debugging) return;
+
+  va_start(ap, format);
+  (void) vsnprintf(buffer, sizeof(buffer), format, ap);
+  va_end(ap);
+  
+  if (foreground) {
+    fprintf (level?stdout:stderr, "%s\n", buffer);
+    return;
+  }
+
+  if (!log_open) {
+    openlog ("LCD4Linux", LOG_PID, LOG_USER);
+    log_open=1;
+  }
+  
+  switch (level) {
+  case 0:
+    syslog (LOG_ERR, "%s", buffer);
+    break;
+  case 1:
+    syslog (LOG_INFO, "%s", buffer);
+    break;
+  default:
+    syslog (LOG_DEBUG, "%s", buffer);
+  }
+}
