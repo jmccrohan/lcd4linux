bc79a5e7ab0c605807e38724983e2b2b664a61d7
reinelt <reinelt@3ae390bd-cb1e-0410-b409-cd5a39f66f1f>
2001-03-02 11:04:08 +0000

    [lcd4linux @ 2001-03-02 11:04:08 by reinelt] cosmetic cleanups (comment headers)
    git-svn-id: https://ssl.bulix.org/svn/lcd4linux/trunk@94 3ae390bd-cb1e-0410-b409-cd5a39f66f1f

--- battery.h
+++ battery.h
@@ -1,6 +1,33 @@
+/* $Id: battery.h,v 1.2 2001/03/02 11:04:08 reinelt Exp $
+ *
+ * APM and ACPI specific functions
+ *
+ * Copyright 2001 by Leo Tötsch (lt@toetsch.at)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *
+ * $Log: battery.h,v $
+ * Revision 1.2  2001/03/02 11:04:08  reinelt
+ *
+ * cosmetic cleanups (comment headers)
+ *
+ */
+
 #ifndef _BATTERY_H_
 #define _BATTERY_H_
-
 
 int Battery ( int *perc, int *stat, double *dur);
 
