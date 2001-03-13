ab548cb3e831ea2443d6362acc319eb01c0ceab6
reinelt <reinelt@3ae390bd-cb1e-0410-b409-cd5a39f66f1f>
2001-03-13 08:03:41 +0000

    [lcd4linux @ 2001-03-13 08:03:41 by reinelt] added missing autoheader files
    git-svn-id: https://ssl.bulix.org/svn/lcd4linux/trunk@115 3ae390bd-cb1e-0410-b409-cd5a39f66f1f

---
+++ acconfig.h
@@ -0,0 +1,33 @@
+/* Define if you want the Beckmann&Egle driver compiled */
+#undef WITH_BECKMANNEGLE
+
+/* Define if you want the Crystalfontz driver compiled */
+#undef WITH_CRYSTALFONTZ
+
+/* Define if you want the HD44780 driver compiled */
+#undef WITH_HD44780
+
+/* Define if you want the Matrix Orbital driver compiled */
+#undef WITH_MATRIXORBITAL
+
+/* Define if you want the PalmPilot driver compiled */
+#undef WITH_PALMPILOT
+
+/* Define if you want the PNG Raster driver compiled */
+#undef WITH_PNG
+
+/* Define if you want the PPM Raster driver compiled */
+#undef WITH_PPM
+
+/* Define if you want the SIN Router driver compiled */
+#undef WITH_SIN
+
+/* Define if you want the Skeleton driver compiled */
+#undef WITH_SKELETON
+
+/* Define if you want the Text driver compiled */
+#undef WITH_TEXT
+
+/* Define if you want the X11 driver compiled */
+#undef WITH_X11
+
