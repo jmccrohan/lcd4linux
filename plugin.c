4c5d6736430c906cc1ed11c759793a23bfc8e962
reinelt <reinelt@3ae390bd-cb1e-0410-b409-cd5a39f66f1f>
2004-06-07 06:56:55 +0000

    [lcd4linux @ 2004-06-07 06:56:55 by reinelt] added test plugin from Andy Baxter
    git-svn-id: https://ssl.bulix.org/svn/lcd4linux/trunk@467 3ae390bd-cb1e-0410-b409-cd5a39f66f1f

--- plugin.c
+++ plugin.c
@@ -1,4 +1,4 @@
-/* $Id: plugin.c,v 1.31 2004/05/29 00:27:23 reinelt Exp $
+/* $Id: plugin.c,v 1.32 2004/06/07 06:56:55 reinelt Exp $
  *
  * plugin handler for the Evaluator
  *
@@ -22,6 +22,10 @@
  *
  *
  * $Log: plugin.c,v $
+ * Revision 1.32  2004/06/07 06:56:55  reinelt
+ *
+ * added test plugin from Andy Baxter
+ *
  * Revision 1.31  2004/05/29 00:27:23  reinelt
  *
  * added plugin_diskstats.c
@@ -182,6 +186,8 @@ int  plugin_init_math   (void);
 void plugin_exit_math   (void);
 int  plugin_init_string (void);
 void plugin_exit_string (void);
+int  plugin_init_test   (void);
+void plugin_exit_test   (void);
 int  plugin_init_time   (void);
 void plugin_exit_time   (void);
 
@@ -232,6 +238,7 @@ int plugin_init (void)
   plugin_init_cfg();
   plugin_init_math();
   plugin_init_string();
+  plugin_init_test();
   plugin_init_time();
   
 #ifdef PLUGIN_APM
@@ -364,6 +371,7 @@ void plugin_exit(void) {
   plugin_exit_cfg();
   plugin_exit_math();
   plugin_exit_string();
+  plugin_exit_test();
   plugin_exit_time();
 
   DeleteFunctions();
---
+++ plugin_test.c
@@ -0,0 +1,116 @@
+/* $Id: plugin_test.c,v 1.1 2004/06/07 06:56:55 reinelt Exp $
+*
+* Handy functions for testing displays and debugging code.
+*
+* Copyright 2004 Andy Baxter.
+* Based on sample plugin by:
+*
+* Copyright 2003 Michael Reinelt <reinelt@eunet.at>
+* Copyright 2004 The LCD4Linux Team <lcd4linux-devel@users.sourceforge.net>
+*
+* This file is part of LCD4Linux.
+*
+* LCD4Linux is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2, or (at your option)
+* any later version.
+*
+* LCD4Linux is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+*
+* $Log: plugin_test.c,v $
+* Revision 1.1  2004/06/07 06:56:55  reinelt
+*
+* added test plugin from Andy Baxter
+*
+*/
+
+int plugin_init_test (void);
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+
+#include "debug.h"
+#include "plugin.h"
+
+#ifdef WITH_DMALLOC
+#include <dmalloc.h>
+#endif
+
+
+// used for testing bars - keeps values for a series of 10 bars,
+// which are incremented and decremented between 0 and rmax by
+// amount rdelta every time they are read. Starting value is rstart.
+// rbar gives the number of the test bar.
+static void my_test_bar (RESULT *result, RESULT *rbar, RESULT *rmax,
+			 RESULT *rstart, RESULT *rdelta)
+{
+  static double values[10]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+  static double deltas[10];
+  int bar;
+  double max,delta,value;
+
+  max=R2N(rmax);
+  delta=R2N(rdelta);
+
+  // the maths is just to stop double rounding errors and bad values.
+  bar=((int)floor(R2N(rbar)+0.1)) % 10;
+  if ( fabs(delta) > 0.1 ) {
+    // don't move or init the bar if delta=0 (the widget is only browsing)
+    if (values[bar] == -1) {
+      // first time called.
+      values[bar]=R2N(rstart);
+      deltas[bar]=delta;
+    };
+    values[bar]+=deltas[bar];
+  };
+  if (values[bar] < 0 || values[bar] > max ) {
+    // turn around.
+    deltas[bar]= -deltas[bar];
+    values[bar]+= deltas[bar];
+  };
+  value=values[bar];
+  SetResult(&result, R_NUMBER, &value);
+}
+
+
+// like above, but just switches a value between 1 and -1. Can use to test
+// visibility of icons.
+static void my_test_onoff (RESULT *result, RESULT *arg1)
+{
+  static int on[10] = {1,1,1,1,1,1,1,1,1,1};
+  int i;
+  double val;
+  
+  i = ((int)floor(R2N(arg1)+0.1)) % 10;
+  on[i] = -on[i];
+  val = (double)on[i];
+
+  SetResult(&result, R_NUMBER, &val);
+}
+
+
+int plugin_init_test (void)
+{
+  
+  AddFunction ("test::bar",   4, my_test_bar);
+  AddFunction ("test::onoff", 1, my_test_onoff);
+  
+  return 0;
+}
+
+void plugin_exit_test(void)
+{
+  // empty
+}
