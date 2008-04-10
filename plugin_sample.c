1780538c9814f3852f7816900a9b3f89b8cd69d9
michux <michux@3ae390bd-cb1e-0410-b409-cd5a39f66f1f>
2008-04-10 14:55:23 +0000

    Add the year 2008 to the copyright
    git-svn-id: https://ssl.bulix.org/svn/lcd4linux/trunk@870 3ae390bd-cb1e-0410-b409-cd5a39f66f1f

--- plugin_sample.c
+++ plugin_sample.c
@@ -4,7 +4,7 @@
  * plugin template
  *
  * Copyright (C) 2003 Michael Reinelt <michael@reinelt.co.at>
- * Copyright (C) 2004, 2005, 2006, 2007 The LCD4Linux Team <lcd4linux-devel@users.sourceforge.net>
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008 The LCD4Linux Team <lcd4linux-devel@users.sourceforge.net>
  *
  * This file is part of LCD4Linux.
  *
